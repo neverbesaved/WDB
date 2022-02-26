3f553b5e565e76d4c787af772aadc6e36977e36a
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-02-26 22:53:17 +0200

    Add work route

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["Code", "Sleep", "Repeat"];
+let workItems =[];
 
 app.set("view engine", "ejs");
 
@@ -19,14 +20,28 @@ app.get("/", function (req, res) {
   };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day: day, items: items });
+  res.render("list", { listTitle: day, items: items });
 });
 
-app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+app.post("/", function (req, res) { 
+     let item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else { 
+        items.push(item);
+        res.redirect("/")
+    }
 });
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List", items:workItems})
+})
+app.post("/work",function(req,res){
+    let item =req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
