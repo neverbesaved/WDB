76b09687cdaf40c020ffab695756a3b28d860703
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-02-26 21:59:18 +0200

    Configure server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,11 +1,32 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app =express();
-app.get("/", function(req,res){
-    res.send("Hello")
-})
+const app = express();
+let items= ["Code","Sleep","Repeat"];
 
-app.listen(3000,function(){
-    console.log("Server is running on port 3000")
-})
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    const options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    let day = today.toLocaleDateString("en-US",options);
+
+    
+    res.render("list", {day: day , items:items});
+});
+
+app.post("/", function(req,res){
+    let item = req.body.newItem;
+    items.push(item);
+   res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
