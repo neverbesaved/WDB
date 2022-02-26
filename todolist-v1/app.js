35f7ee93b882cb0edc8d7a0f3ffd7b5e07798d67
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-02-26 22:26:54 +0200

    Add styling

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,29 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-let items= ["Code","Sleep","Repeat"];
+let items = ["Code", "Sleep", "Repeat"];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let today = new Date();
-    const options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-    let day = today.toLocaleDateString("en-US",options);
+  let today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
 
-    
-    res.render("list", {day: day , items:items});
+  res.render("list", { day: day, items: items });
 });
 
-app.post("/", function(req,res){
-    let item = req.body.newItem;
-    items.push(item);
-   res.redirect("/");
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
