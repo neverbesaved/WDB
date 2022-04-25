899235bf22a31c99fb2919424cabc053c98c960d
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-04-25 19:47:34 +0300

    Change mongoose connection

--- todolist-v2-starting-files/todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/todolist-v2-starting-files/app.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://neverbesaved:bordelans23@cluster0.l5so9.mongodb.net/todolistDB");
+mongoose.connect("mongodb+srv://login:password@cluster0.l5so9.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String,
