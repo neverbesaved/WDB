304995f93ef1611fe5dbf2ff80be039aae40bf36
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-04-25 22:49:29 +0300

    Set POST route

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -34,6 +34,20 @@ app.get("/articles", (req,res) => {
     })
 })
 
+app.post("/articles", (req,res) => { 
+    const newArticle = new Article ({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save((err) => {
+        if(!err){
+            res.send("Success!")
+        } else { 
+            res.send(err);
+        }
+    });
+})
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
