40b39d038c2eb3671119ab3025d76c010b1af0b8
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-05-05 14:23:52 +0300

    Remove nodu-modules folder

--- Auth-module/app.js
+++ Auth-module/app.js
@@ -11,11 +11,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String,
+  password: String
 });
 
 const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
