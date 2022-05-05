30604a0088c720e94cbec92ad1e407da21c0c5b7
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-05-05 14:15:14 +0300

    Add db encryption (lvl 2)

--- Auth-module/app.js
+++ Auth-module/app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -61,8 +65,8 @@ app.post("/login", (req, res) => {
     if (foundUser) {
       if (foundUser.password === password) {
         res.render("secrets");
-      } else { 
-          console.log("Password is wrong");
+      } else {
+        console.log("Password is wrong");
       }
     }
   });
