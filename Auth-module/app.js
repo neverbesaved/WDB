9897ab202a1a022c790a1371873c740c50861a1a
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-05-06 20:06:08 +0300

    Upgrade hash algorithm from md5 to bcrypt

--- Auth-module/app.js
+++ Auth-module/app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const bcrypt = require("bcrypt");
+const saltRounds = 15;
 
 const app = express();
 
@@ -12,8 +13,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
-}); 
+  password: String,
+});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,33 +40,37 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
     }
     if (foundUser) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      } else {
-        console.log("Password is wrong");
-      }
+      bcrypt.compare(password, foundUser.password, (err, result) => {
+        if (result) {
+          res.render("secrets");
+        } else {
+          console.log("Password is wrong");
+        }
+      });
     }
   });
 });
