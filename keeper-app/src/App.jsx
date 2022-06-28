c639b4911257eb74dc43c8d6aa98279fd31f6906
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-06-29 00:00:24 +0300

    Create Note component , render on page

--- keeper-app/src/App.jsx
+++ keeper-app/src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
+import notes from "./notes"
 
 function App() {
 
   return (
     <div >
     <Header/>
-    <Note/>
+    {notes.map((note) =><Note key={note.id} title={note.title} content={note.content}  />)}
     <Footer/>
     </div>
   )
--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
   return (
     <div className="bg-[#fff] rounded-[7px] shadow-[0_2px_5px_[#ccc]] p-[10px] w-[240px] m-4 float-left">
-      <h1 className="text-[1.1em] mb-[6px]">Note</h1>
-      <p className="text-[1.1em] mb-[10px] whitespace-pre-wrap break-words">desciprtion</p>
+      <h1 className="text-[1.1em] mb-[6px]">{props.title}</h1>
+      <p className="text-[1.1em] mb-[10px] whitespace-pre-wrap break-words">{props.content}</p>
     </div>
   );
 };
